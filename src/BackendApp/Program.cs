25a8a28f827316ac28a721cadfcf3be0fef6d6fe
Мельников Андрей Александрович <melnikov.aaleksan@devzone.local>
2024-11-08 00:59:39 +0500

    chore: init repo

---
+++ src/BackendApp/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.MapControllers();
+
+app.Run();
+
+public partial class Program { }
